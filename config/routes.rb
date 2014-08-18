d7114f7a0c046381dd4661ba07a0264190268bfa
Antonio Misaka <amisaka@gmail.com>
2014-08-18 15:43:03 -0400

    Changed Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SampleApp::Application.routes.draw do
   get "static_pages/contact"
   get "static_pages/blogs"
   get "static_pages/press"
+
+  root to: 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
